e2f07a08a6b17081d0767de15c47cbd141a27c77
fayshal69 <foyshalbinamir@gmail.com>
2025-01-10 00:00:08 +0600

    added: protected route

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
 
 type TAuthState = {
   user: null | object;
@@ -26,6 +27,9 @@ const authSlice = createSlice({
     },
   },
 });
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const useCurrentUser = (state: RootState) => state.auth.user;
 
 export const { setUser, logout } = authSlice.actions;
 
